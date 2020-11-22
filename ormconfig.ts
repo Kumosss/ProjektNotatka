496eab708404311f155a34e5fc7d2a90a7bf4f43
Mateusz Kumosinski <mateusz.kumosinski@gmail.com>
2020-11-22 23:13:52 +0100

    kolejna próba naprawienia azure

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,17 +2,12 @@ import { join } from 'path';
 import { ConnectionOptions } from 'typeorm';
 
 export const ormconfig: ConnectionOptions = {
-  name: 'default',
-  type: 'postgres',
-  host: 'projektnotatki.postgres.database.azure.com',
-  port: 5432,
-  username: 'kumos@projektnotatki',
-  password: 'BGT%6yhn',
-  database: 'postgres',
-  synchronize: false,
-  cache: false,
-  logger: 'debug',
-  migrations: ['dist/migrations/**/*.js'],
-  entities: [join(__dirname, '**', '*.entity.js')],
-  migrationsTableName: 'migrations',
+    type: process.env.TYPEORM_CONNECTION as any,
+    host: process.env.TYPEORM_HOST,
+    port: Number(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE),
+    entities: [join(__dirname, '**', '*.entity.js')]
 };
